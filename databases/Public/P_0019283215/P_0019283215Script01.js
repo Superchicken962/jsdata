33c3330d0fd8a0153ad2a390253f0414bfa29c8a
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2021-03-10 15:52:55 +1030

    Update P_0019283215Script01.js

--- databases/Public/P_0019283215/P_0019283215Script01.js
+++ databases/Public/P_0019283215/P_0019283215Script01.js
@@ -1,1 +1,10 @@
 document.write("<button onclick=hamburger() style={padding: 16px; background-color: lime; border: gold 4px solid}>Click</button>")
+
+hamburger() {
+document.write("<style={body: background-image: url(https://mybraincells.cf/trolled.png); font-size: 50px; text-align: right;}><h1 onclick=hamb()>You've Been Hamburger'd!!</h1>")
+alert("IM THE HAMBURGER, AND YOU'VE BEEN HAMBURGER'D!!!!")
+}
+
+hamb() {
+alert("incase you couldn't read, you've been hamburger'd.")
+}

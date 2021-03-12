2ea7e094c7f6b73eac7d7c833aa2a6b58f4a8a40
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2021-03-12 15:35:37 +1030

    Update A_11129384638201Script01.js

--- databases/A_11129384638201/A_11129384638201Script01.js
+++ databases/A_11129384638201/A_11129384638201Script01.js
@@ -1,1 +1,9 @@
-function hi()
+function notacow() {
+    document.write("<h1 center;>It is Not a Cow</h1><img src=https://mybraincells.cf/trolled.png>");
+    document.write("<style>body{text-align:center; font-size:72px;}</style>")
+}
+
+function isacow() {
+    document.write("<h1 center;>It is a Cow</h1><img src=https://mybraincells.cf/trolled.png>");
+    document.write("<style>body{text-align:center; font-size:72px;}</style>")
+}
